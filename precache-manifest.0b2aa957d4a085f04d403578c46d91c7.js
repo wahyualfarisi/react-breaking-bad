46dccdb691b00c7d4ea98c4d8b452c0927b091df
wahyu alfarisi <wahyualfarisi@gmail.com>
2020-07-25 17:10:03 +0700

    Updates

---
+++ precache-manifest.0b2aa957d4a085f04d403578c46d91c7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc0f606e2f4b1ddab038d1c30d91c82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdf400d7a9b5262182c3",
+    "url": "/static/css/main.3b5b1726.chunk.css"
+  },
+  {
+    "revision": "6de3f82cda0432a34bff",
+    "url": "/static/js/2.31262107.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.31262107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdf400d7a9b5262182c3",
+    "url": "/static/js/main.49a8a406.chunk.js"
+  },
+  {
+    "revision": "a964bc697d914d59f04e",
+    "url": "/static/js/runtime-main.c13a616d.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/static/media/spinner.235b9b3e.gif"
+  }
+]);
